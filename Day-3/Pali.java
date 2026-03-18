08b5e33a3a73e3647420ef7df70f38fbedba2405
Mukesh <mukesh1152006@gmail.com>
2026-03-18 19:58:40 +0530

    Add files via upload

---
+++ Day-3/Pali.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Pali{
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int or=n;
+
+        int rev=0;
+
+        while(n>0) {
+            int d=n%10;
+            rev=rev*10+d;
+            n=n/10;
+        }
+
+        if(or==rev) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
