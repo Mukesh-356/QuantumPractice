a91dd7ebbad298140927395d0118390916c1b047
Mukesh <mukesh1152006@gmail.com>
2026-03-26 21:39:43 +0530

    Add files via upload

---
+++ Day-9/Fib.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Fib{
+    public static int fibon(int n) 
+    {
+        if(n==0){ 
+            return 0;
+        }
+        if(n==1) {
+            return 1;
+        }
+        
+        return fibon(n-1)+fibon(n-2);
+    }
+  public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibon(n));
+    }
+}
